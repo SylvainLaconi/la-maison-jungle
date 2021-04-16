cf83d41170a15b4f3d26c71fff957b601682465f
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-04-16 16:41:16 +0200

    P3C2 done

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const Categories = () => {
-  return <div></div>;
+const Categories = ({ categories, setCategorie }) => {
+  return (
+    <div>
+      <select
+        name="category-selector"
+        id="category-selector"
+        onChange={(e) => setCategorie(e.target.value)}
+      >
+        {categories.map((cat) => (
+          <option key={cat} value={cat}>
+            {cat}
+          </option>
+        ))}
+      </select>
+      <button onClick={() => setCategorie(categories)}>Réinitialiser</button>
+    </div>
+  );
 };
 
 export default Categories;
